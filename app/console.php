76cfde2572b6f7c3fc13bccadfb73f01ae3c1880
Mickael ANDRIEU <mickael.andrieu@lp-digital.fr>
2015-09-23 13:43:37 +0200

    added command sample

--- app/console.php
+++ app/console.php
@@ -3,8 +3,19 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 $console = new Application('Jarvis - Symfony Flavored Edition', '0.1');
 
+$console
+    ->register('hello:jarvis')
+    ->setDescription('The Hello World command from Jarvis')
+    ->setCode(function (InputInterface $input, OutputInterface $output) {
+        $output = new SymfonyStyle($input, $output);
+        $output->success('Hello World from Jarvis Symfony Flavored Edition !');
+    })
+;
 
 return $console->run();
